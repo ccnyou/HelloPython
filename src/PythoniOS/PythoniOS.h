13e981d36189746aca1d6a1a369cb004db98562d
ervinchen <ccnyou@qq.com>
2017-06-02 22:08:13 +0800

    * 用 lib 方式，不用 framework

---
+++ src/PythoniOS/PythoniOS.h
@@ -0,0 +1,19 @@
+//
+//  PythoniOS.h
+//  PythoniOS
+//
+//  Created by ervinchen on 2017/6/2.
+//  Copyright © 2017年 Tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PythoniOS.
+FOUNDATION_EXPORT double PythoniOSVersionNumber;
+
+//! Project version string for PythoniOS.
+FOUNDATION_EXPORT const unsigned char PythoniOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PythoniOS/PublicHeader.h>
+
+
